71806e3df1bf9278048e954142755cc649d5b6f6
Peter Schwenk <paschwenk@gmail.com>
2025-12-01 17:45:17 -0500

    After adding improved report format with sorted character counts

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words, count_characters
+from stats import count_words, count_characters, sort_character_counts
 
 def get_book_text(book_path):
     with open(book_path) as f:
@@ -9,9 +9,17 @@ def main():
     book_text = get_book_text(book_path)
     num_words = count_words(book_text)
     char_counts = count_characters(book_text)
+    sorted_char_counts = sort_character_counts(char_counts)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
-    print("Character counts:")
-    print(char_counts)
+    print("--------- Character Count -------")
+    for entry in sorted_char_counts:
+        char = entry["char"]
+        count = entry["num"]
+        print(f"{char}: {count}")
+    print("============= END ===============")
     
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -6,8 +6,16 @@ def count_characters(text):
     char_counts = {}
     for char in text:
         char = char.lower()
-        if char in char_counts:
-            char_counts[char] += 1
-        else:
-            char_counts[char] = 1
+        if char.isalpha():
+            if char in char_counts:
+                char_counts[char] += 1
+            else:
+                char_counts[char] = 1
     return char_counts
+
+def sort_character_counts(char_counts):
+    result = []
+    for char, count in char_counts.items():
+        result.append({"char": char, "num": count})
+    result.sort(key=lambda x: x["num"], reverse=True)
+    return result
